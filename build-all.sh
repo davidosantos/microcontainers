033d8d8e1b957307c57a26a7ff985d2da7f422d5
Your Name <you@example.com>
2023-07-24 00:41:37 -0300

    .

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 # Instalar K3S com kubectl
+
+wait_for_pods() {
+  local NAMESPACE="$1"
+
+  while true; do
+    PODS_RUNNING=$(kubectl get pods -n "$1" --no-headers | awk '{print $3}' | grep -v "Running")
+
+    if [ -z "$PODS_RUNNING" ]; then
+      echo "All Pods are running in namespace $1."
+      break
+    else
+      echo "Waiting for Pods to be in the 'Running' state in namespace $1..."
+      sleep 1
+    fi
+  done
+}
+
 curl -sfL https://get.k3s.io | sh -
 # Habilitar permissão para usuário ubuntu obter configuração de acesso ao K8S
 echo K3S_KUBECONFIG_MODE=\"644\" >> /etc/systemd/system/k3s.service.env
@@ -17,18 +34,4 @@ wait_for_pods "default"
 
 
 
-wait_for_pods() {
-  local NAMESPACE="$1"
 
-  while true; do
-    PODS_RUNNING=$(kubectl get pods -n "$1" --no-headers | awk '{print $3}' | grep -v "Running")
-
-    if [ -z "$PODS_RUNNING" ]; then
-      echo "All Pods are running in namespace $1."
-      break
-    else
-      echo "Waiting for Pods to be in the 'Running' state in namespace $1..."
-      sleep 1
-    fi
-  done
-}
